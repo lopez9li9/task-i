b85f32518e2a2819166450b161a82044f450a6d5
Lopez9li9 <isaiasrlopez13@gmail.com>
2023-05-13 01:48:41 -0500

    modify: post request ok

--- server/src/controllers/stage.controller.ts
+++ server/src/controllers/stage.controller.ts
@@ -1,26 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 
 import Stage from '../models/stage.model';
+import Team from '../models/team.model';
 
 import { BadRequest, Conflict, NotFound } from '../helpers/custom.errors';
-import { IStage } from '../interfaces/models.interfaces';
+import { IStage, ITeam } from '../interfaces/models.interfaces';
+import { arraysEqual } from '../utils';
 
 export const getStage = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const { name } = request.query;
     let query: any = {};
 
-    if (name) {
-      query = { name: { $regex: name.toString(), $options: 'i' } };
-    }
+    if (name) query = { name: { $regex: name.toString(), $options: 'i' } };
 
     query.status = { $ne: false };
 
-    const stages: IStage[] = await Stage.find(query).populate('team', 'name');
+    const stages: IStage[] = await Stage.find(query);
 
-    if (!stages.length) {
-      throw new NotFound(name ? `Stage with name ${name} not found` : 'Stages not found');
-    }
+    console.log(stages);
+    /**add populate('team', 'name') */
+
+    if (!stages.length) throw new NotFound(name ? `Stage with name ${name} not found` : 'Stages not found');
 
     response.status(200).json(stages);
   } catch (error) {
@@ -32,16 +33,27 @@ export const createStage = async (request: Request, response: Response, next: Ne
   try {
     const { name, teams } = request.body;
 
-    if (!name || !teams) throw new BadRequest('Name and teams are required');
+    if (!name) throw new BadRequest('Name is required');
 
-    const existingStage: IStage | null = await Stage.findOne({ name, status: true });
+    const existingStage: IStage | null = await Stage.findOne({ name });
+    if (existingStage) throw new BadRequest(`Stage with name ${name} already exists`);
 
-    if (existingStage) throw new Conflict(`Stage with name ${name} already exists`);
+    let teamsIds: ITeam['id'][] = [];
 
-    const stage: IStage = new Stage({ name, teams, status: true });
-    await stage.save();
+    if (Array.isArray(teams) && teams.length > 0) {
+      for (const teamName of teams) {
+        const team: ITeam | null = await Team.findOne({ name: teamName });
 
-    response.status(201).json(stage);
+        if (!team) throw new NotFound(`Team with name ${teamName} not found`);
+
+        teamsIds.push(team._id);
+      }
+    }
+
+    const newStage: IStage = new Stage({ name, teams: teamsIds });
+    await newStage.save();
+
+    response.status(201).json(newStage);
   } catch (error) {
     next(error);
   }
@@ -50,26 +62,26 @@ export const createStage = async (request: Request, response: Response, next: Ne
 export const updateStage = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const { id } = request.params;
-
     if (!id) throw new BadRequest('Id is required');
+
+    const stage: IStage | null = await Stage.findById(id);
+    if (!stage) throw new NotFound('Stage not found');
 
     const { name, teams } = request.body;
 
-    if (!name || !teams) throw new BadRequest('At least one field is required');
+    if (!name && !teams) throw new BadRequest('At least one field is required');
 
-    const stage: IStage | null = await Stage.findOne({ id, status: true });
+    if (name) {
+      if (name === stage.name) throw new Conflict('Name cannot be the same');
 
-    if (!stage) throw new NotFound('Stage not found');
-
-    if (name === stage.name) throw new Conflict('Name cannot be the same');
-
-    name && (stage.name = name);
-
-    if (teams.length === stage.teams.length && teams.every((team: string) => stage.teams.includes(team))) {
-      throw new Conflict('Teams cannot be the same');
+      stage.name = name;
     }
 
-    teams && (stage.teams = teams);
+    if (teams) {
+      if (arraysEqual(teams, stage.teams)) throw new Conflict('Teams cannot be the same');
+
+      stage.teams = teams;
+    }
 
     await stage.save();
 
--- server/src/models/stage.model.ts
+++ server/src/models/stage.model.ts
@@ -4,7 +4,7 @@ import { IStage } from '../interfaces/models.interfaces';
 
 const stageSchema = new Schema<IStage>({
   name: { type: String, required: true, unique: true },
-  teams: { type: [Types.ObjectId], ref: 'Team', required: true },
+  teams: { type: [Types.ObjectId], ref: 'Team', default: [] },
   status: { type: Boolean, default: true },
 });
 
